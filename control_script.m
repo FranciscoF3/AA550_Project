2708c8be6c3bb7a53ee2267ce20322d464f1a9c3
FranciscoF3 <156865458+FranciscoF3@users.noreply.github.com>
2025-11-21 15:28:12 -0800

    Add files via upload

--- control_script.m
+++ control_script.m
@@ -77,13 +77,13 @@ function target_ref = reference_sampling(ref, robot)
 end
 
 function [Ad, Bd, Cd] = linearize_discretize(z_r, u_r, dt, params)
-
-[A_r, B_r, f_r] = compute_jacobians(z_r, u_r, params);
-
-C_r = f_r - A_r*z_r -B_r*u_r;
-
-Ad = m
-
+    
+    [A_r, B_r, f_r] = compute_jacobians(z_r, u_r, params);
+    
+    C_r = f_r - A_r*z_r -B_r*u_r;
+    nz = length(z_r);
+    nu = length(u_r);
+    
     % Extract blocks
     Ad = Md(1:nz, 1:nz);
     Bd = Md(1:nz, nz+1:nz+nu);
@@ -94,11 +94,31 @@ function [A_r, B_r, f_r] = compute_jacobians(z_r, u_r, params)
 
 %Compute_Jacobians
 
-K = v_x*cos(e_phi)-v_y*sin*(e_phi);
+    %state
+    ey   = z_r(1);
+    ephi = z_r(2);
+    vx   = z_r(3);
+    vy   = z_r(4);
+    omega = z_r(5);
+    ax   = z_r(6);
+    ay   = z_r(7);
 
-    A_r = 
-    
-    B_r =
+    %input
+    alpha  = u_r(1);
+    jx = u_r(2);
+    jy = u_r(3);
+
+    % path radius
+    rho_s = params.rho_s;
+
+    % helper K
+    K = vx*cos(ephi)-vy*sin(ephi);
+
+    nz = length(z_r);
+    nu = length(u_r);
+
+    A_r = zeros(nz);
+    B_r = zeros(nz, nu);
 
     %%% f_1 = e_y %%
     A_r(1,1) = ((rho_s - ey)*(vx*sin(ephi) + vy*cos(ephi))^2)/(rho_s^2*K^2) ...
@@ -107,30 +127,29 @@ K = v_x*cos(e_phi)-v_y*sin*(e_phi);
                - 2*vx*vy*sin(ephi)*cos(ephi))/K^2;
     A_r(1,3) = (rho_s - ey)/(rho_s*K^2);
     A_r(1,4) = -(vy)/(K^2);
-    A_r(1,5) = -omega/(rho_s*K);
+    
     
     
     %%% f_2 = e_phi %%
-    A_r(2,1) = omega*(rho_s - ey)*(vx*sin(ephi) + vy*cos(ephi))/(rho_s*K^2);
-    A_r(2,2) = ((rho_s - ey)*(vx^2 + vy^2)*(sin(ephi)^2 - cos(ephi)^2) ...
-                - 2*vx*vy*sin(ephi)*cos(ephi))/(K^2);
+    A_r(2,1) = -omega/(rho_s*K);
+    A_r(2,2) = omega*(rho_s - ey)*(vx*sin(ephi) + vy*cos(ephi))/(rho_s*K^2);
     A_r(2,3) = -omega*(rho_s - ey)*cos(ephi)/(rho_s*K^2);
     A_r(2,4) = omega*(rho_s - ey)*sin(ephi)/(rho_s*K^2);
     A_r(2,5) = (rho_s - ey)/(rho_s*K);
     
     %%% f_3 = v_x %%
-    A_r(3,1) = -a_x/(rho_s*K);
-    A_r(3,2) = a_x*(rho_s - ey)*(vx*sin(ephi) + vy*cos(ephi))/(rho_s*K^2);
-    A_r(3,3) = -a_x*(rho_s - ey)*cos(ephi)/(rho_s*K^2);
-    A_r(3,4) = a_x*(rho_s - ey)*sin(ephi)/(rho_s*K^2);
-    A_r(3,5) = (rho_s- e_y)/ (rho_s*K);
+    A_r(3,1) = -ax/(rho_s*K);
+    A_r(3,2) = ax*(rho_s - ey)*(vx*sin(ephi) + vy*cos(ephi))/(rho_s*K^2);
+    A_r(3,3) = -ax*(rho_s - ey)*cos(ephi)/(rho_s*K^2);
+    A_r(3,4) = ax*(rho_s - ey)*sin(ephi)/(rho_s*K^2);
+    A_r(3,6) = (rho_s- e_y)/ (rho_s*K);
     
     %%% f_4 = v_y %%
     A_r(4,1) = -ay/(rho_s*K);
     A_r(4,2) = ay*(rho_s - ey)*(vx*sin(ephi) + vy*cos(ephi)) / (rho_s*K^2);
     A_r(4,3) = -ay*(rho_s - ey)*cos(ephi) / (rho_s*K^2);
     A_r(4,4) =  ay*(rho_s - ey)*sin(ephi) / (rho_s*K^2);
-    A_r(4,5) = (rho_s - ey) / (rho_s*K);
+    A_r(4,6) = (rho_s - ey) / (rho_s*K);
     
     %%% f_5 = omega %%
     A_r(5,1) = -alpha/(rho_s*K);
@@ -139,20 +158,23 @@ K = v_x*cos(e_phi)-v_y*sin*(e_phi);
     A_r(5,4) =  alpha*(rho_s - ey)*sin(ephi) / (rho_s*K^2);
     B_r(5,1) = (rho_s - ey) / (rho_s*K);
     
-    %%% f_6 = a_x %%
+    %%% f_6 = ax %%
     A_r(6,1) = -jx/(rho_s*K);
     A_r(6,2) = jx*(rho_s - ey)*(vx*sin(ephi) + vy*cos(ephi)) / (rho_s*K^2);
     A_r(6,3) = -jx*(rho_s - ey)*cos(ephi) / (rho_s*K^2);
     A_r(6,4) =  jx*(rho_s - ey)*sin(ephi) / (rho_s*K^2);
-    A_r(6,5) =  (rho_s-ey) / (rho_s*K);
+    B_r(6,2) =  (rho_s-ey) / (rho_s*K);
 
     %%% f_7 = a_y %%
     A_r(7,1) = -jy/(rho_s*K);
-    A_r(7,2) = (rho_s - ey) / rho_sK;
-    A_r(7,3) = jy*(rho_s - ey)*(vx*sin(ephi) + vy*cos(ephi)) / (rho_s*K^2); 
-    A_r(7,4) = -jy*(rho_s - ey)*cos(ephi) / (rho_s*K^2);
-    A_r(7,5) =  jy*(rho_s - ey)*sin(ephi) / (rho_s*K^2);
+    B_r(7,3) = (rho_s - ey) / rho_s*K;
+    A_r(7,2) = jy*(rho_s - ey)*(vx*sin(ephi) + vy*cos(ephi)) / (rho_s*K^2); 
+    A_r(7,3) = -jy*(rho_s - ey)*cos(ephi) / (rho_s*K^2);
+    A_r(7,4) =  jy*(rho_s - ey)*sin(ephi) / (rho_s*K^2);
     
+ 
+    f_r = zeros(nz,1);
+end
 
 
 
